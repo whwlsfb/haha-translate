a93cef9dcfd825596d088132e8bd6ba1aad76941
whwlsfb <whwlsfb@wanghw.cn>
2021-08-12 13:43:57 +0800

    init.

---
+++ morse.js
@@ -0,0 +1,22 @@
+// 编码
+$("#encode").click(function () {
+  $("#result").val(xmorse.encode($("#input").val(), getoption()));
+  $("#play").show();
+});
+
+// 解码
+$("#decode").click(function () {
+  $("#result").val(
+    xmorse.decode($("#input").val(), getoption()) ||
+      "解码失败，请确认输入是否正确"
+  );
+  $("#play").hide();
+});
+
+function getoption() {
+  return {
+    space: " ",
+    short: "？",
+    long: "哈",
+  };
+}
